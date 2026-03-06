076f259451718fcbed93e9458812efeb9e50c8b0
AlanaCorso <alana.corso@aluno.senai.br>
2026-03-06 11:28:34 -0300

    Commit inicial: Estrutura HTML e CSS adicionada e README atualizado

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-// 1ª Digitação (JS Completo)
+// ================== CONTROLE DO MENU MOBILE ==============================
+const menuIcon = document.querySelector('#menu-icon');
+const navList = document.querySelector('.navList');
+
+menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('bx-x');
+    navList.classList.toggle('open');
+
+    // Bloquear scroll quando menu aberto
+});
